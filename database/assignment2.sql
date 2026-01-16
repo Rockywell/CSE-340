ca54823b6ace8be1ed15d9056361cd50e266855f
Rockywell <P8errockwell@gmail.com>
2026-01-16 12:38:46 -0700

    Completed Assignment 2 and added the setup video.

---
+++ database/assignment2.sql
@@ -0,0 +1,29 @@
+INSERT INTO public.account (
+    account_firstname,
+    account_lastname,
+    account_email,
+    account_password
+  )
+VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+
+UPDATE public.account
+SET account_type = 'Admin'
+WHERE account_id = 1;
+
+DELETE FROM public.account
+WHERE account_id = 1;
+
+
+UPDATE public.inventory
+SET inv_description = REPLACE(inv_description, 'the small interiors', 'a huge interior')
+WHERE inv_id = 10;
+
+SELECT inv_make, inv_model, classification_name FROM public.inventory as i
+INNER JOIN public.classification as c
+ON c.classification_id = i.classification_id
+WHERE c.classification_name = 'Sport';
+
+UPDATE public.inventory
+SET 
+	inv_image = REPLACE(inv_image, 'images', 'images/vehicles'),
+	inv_thumbnail = REPLACE(inv_thumbnail, 'images', 'images/vehicles');
